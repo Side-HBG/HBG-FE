f94cb967b9f8013f387f2aec4fb3473f1b07d84c
JeongGyun <njk3397@gmail.com>
2024-03-31 15:59:32 +0900

    commit test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p> HBG-FE Test : NJK</p>
+    <p> HBG-FE Test : NJK 0331
+    </p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
