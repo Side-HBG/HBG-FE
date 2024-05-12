5db5a9439563b8a4b91340cd6e00e42830e7f56b
JeongGyun <njk3397@gmail.com>
2024-05-12 18:06:16 +0900

    0512-화면수정?

---
+++ src/views/common/commonFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    <p>https://github.com/Side-HBG</p>
+  </footer>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/common/commonHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <div id="nav">
+      <router-link class="menu" to="/">Main</router-link> |
+      <router-link class="menu" to="/login">Login</router-link>
+    </div>
+  </header>
+</template>
+
+
+<script setup>
+</script>
