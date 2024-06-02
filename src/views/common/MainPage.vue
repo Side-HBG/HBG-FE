66d7a3627418f4398c15a9911d478a00555ddcfb
JeongGyun <njk3397@gmail.com>
2024-06-02 16:06:15 +0900

    game Search List 출력

--- src/views/common/MainPage.vue
+++ src/views/common/MainPage.vue
@@ -7,11 +7,12 @@
       <input type="text" v-model="txtSearchGameName" @keyup.enter="searchGame()" placeholder="게임 검색하기">
       <button id="btnClick" name="btnClick" @click="searchGame()" >검색</button>
 
-
-      <p>{{gameName}}</p>
-      <p>{{gamePrice}}</p>
-      <p>{{gameDisCountPercent}}</p>
-      <p>{{gameTotalPrice}}</p>
+      <li v-for="game in gameList" v-bind:key="game.gameName">
+        <p>{{game.name}}</p>
+        <p>{{game.initial}}</p>
+        <p>{{game.discount_percent}}</p>
+        <p>{{game.price}}</p>
+      </li>
     </div>
   </body>
 </template>
@@ -31,10 +32,14 @@
     name:"App",
     data(){
       return{
-        gameName: "name",
-        gamePrice: 0,
-        gameDisCountPercent: 0,
-        gameTotalPrice: 0,
+        gameList:[
+          /*{
+            gameName: "name",
+            gamePrice: 0,
+            gameDisCountPercent: 0,
+            gameTotalPrice: 0,
+          }*/
+        ]
       };
     },
     methods:{
@@ -43,21 +48,30 @@
         //URL += this.txtSearchGameName;
         axios.get(sendUrl)
           .then((result) => {
-            const game = result.data.data;
+            // alert(typeof result);
+            const game = result.data.data[0];
 
+            // const game = result.data.data[0];
+
+            alert(JSON.stringify(result));
+            alert(JSON.stringify(result.data.data[0]));
+            // alert(result);
             if(result.data.result === true && result.data.statusCode === 200){
+              this.gameList = result.data.data;
+              /*
               this.gameName = game.name;
               this.gamePrice = game.initial;
               this.gameDisCountPercent = game.discount_percent + "%";
-              this.gameTotalPrice = game.price;
+              this.gameTotalPrice = game.price;*/
 
-            }
-            if(result.data.statusCode === 500){
-              this.gameName = "검색된 게임이 존재하지 않습니다.";
+            }else if(result.data.statusCode === 500){
+              /*this.gameName = "검색된 게임이 존재하지 않습니다.";
               this.gamePrice = null;
               this.gameDisCountPercent = null;
-              this.gameTotalPrice = null;
+              this.gameTotalPrice = null;*/
              // this.$router.push("/500").catch(() => {});
+            }else{
+              this.gameName = JSON.stringify(game);
             }
           })
           .catch(function(error){
